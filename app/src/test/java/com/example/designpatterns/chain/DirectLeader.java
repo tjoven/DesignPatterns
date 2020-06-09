10f81e0edbd75ef9d648339ab9d58a8d98564d10
zhaowen.tian <zhaowen.tian@ucarinc.com>
2020-06-09 17:21:36 +0800

    责任链模式

---
+++ app/src/test/java/com/example/designpatterns/chain/DirectLeader.java
@@ -0,0 +1,19 @@
+package com.example.designpatterns.chain;
+
+public class DirectLeader extends LeaveHandler {
+    @Override
+    public int handler(LeaveRequest request) {
+        if(request.days < MIN){
+            System.out.println("DirectLeader 同意");
+            return CODE_AGREE;
+        }
+        System.out.println("请示经理");
+        if(nextHandler != null){
+            return nextHandler.handler(request);
+        }else {
+            System.out.println("不同意");
+            return CODE_DISAGREE;
+        }
+
+    }
+}
